b266a8b52232183ff96a81fd976e43445b57a376
asalzp <71600869+asalzp@users.noreply.github.com>
2021-08-31 21:51:48 -0230

    Add files via upload

---
+++ Malefiz Game Project/Group's Iteration 4/FactorsTester.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+class FactorsTester {
+
+	@Test
+	void testPerfect1()
+	{	
+		// TEST 1: should throw the exception because the parameter value is less than 1
+		assertThrows(IllegalArgumentException.class, () -> FactorsUtility.perfect(0));
+	}
+	
+	@Test
+	void testPerfect2()
+	{	
+		// TEST 2: should succeed because 1 is a valid parameter value, but is not a perfect number
+		assertFalse(FactorsUtility.perfect(1));
+	}
+	
+	@Test
+	void testPerfect3()
+	{	
+		// TEST 3: should succeed because 6 is a valid parameter value, and is a perfect number
+		assertTrue(FactorsUtility.perfect(6));
+	}
+	
+	@Test
+	void testPerfect4()
+	{	
+		// TEST 4: should succeed because 7 is a valid parameter value, but is not a perfect number
+		// I've coded this using assertEquals to show that there's often more than one way to write a test 
+		boolean expected = false;
+		assertEquals(expected, FactorsUtility.perfect(7));
+	}
+
+}
